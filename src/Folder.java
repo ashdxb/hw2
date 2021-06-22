b69d3aff120f2642c43d52e3979f890e0b11c1d0
ashdxb <yoav19991@gmail.com>
2021-06-22 18:19:12 +0300

    init

--- src/Folder.java
+++ src/Folder.java
@@ -1,11 +1,22 @@
-public class Folder {
-    public Folder(String c) {
+import java.util.ArrayList;
+
+public class Folder extends StorageItem {
+
+    private ArrayList<StorageItem> folderContents;
+    private String folderName;
+
+    public Folder(String folderName) {
+        super(folderName);
+        this.folderContents = new ArrayList<StorageItem>();
     }
 
-    public void addItem(File fi1) {
+    @Override
+    public int getSize() {
+        return 0;
     }
 
-    public void addItem(Folder fo1) {
+    public void addItem(StorageItem fi1) {
+        folderContents.add(fi1);
     }
 
     public File findFile(String path) {
