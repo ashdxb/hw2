f42146f6cb29845014f25a0c4ffef933c6ee19f9
Bar <bar.forkosh@campus.technion.ac.il>
2021-06-22 18:18:18 +0300

    1

--- src/Folder.java
+++ src/Folder.java
@@ -1,22 +1,11 @@
-import java.util.ArrayList;
-
-public class Folder extends StorageItem {
-
-    private ArrayList<StorageItem> folderContents;
-    private String folderName;
-
-    public Folder(String folderName) {
-        super(folderName);
-        this.folderContents = new ArrayList<StorageItem>();
+public class Folder {
+    public Folder(String c) {
     }
 
-    @Override
-    public int getSize() {
-        return 0;
+    public void addItem(File fi1) {
     }
 
-    public void addItem(StorageItem fi1) {
-        folderContents.add(fi1);
+    public void addItem(Folder fo1) {
     }
 
     public File findFile(String path) {
